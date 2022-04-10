eb22083ba248a2429ee8390663480cd4dd188df6
Ayache Khettar <ayache_khettar@vfc.com>
2022-04-10 19:41:53 +0100

    removing cred

--- go.mod
+++ go.mod
@@ -5,10 +5,15 @@ go 1.18
 require (
 	github.com/dghubble/go-twitter v0.0.0-20220319054129-995614af6514
 	github.com/dghubble/oauth1 v0.7.1
+	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a
 )
 
 require (
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,39 +6,93 @@ import (
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
-func main() {
-	// consumerKey := "o37I7QrJe6Jf7DqRuT7hHeuCo"
-	// consumerSecret := "1HglkuCH30gceeSPGiFeSS4j5m3WGkUEeE92P09eN2tnUAJxKU"
-	// accessToken := "1508039357898317828-5FsffejCmYX7VPdYqJNwRqcNONWqD9"
-	// accessSecret := "gJo0LxhlPzyQfxW2oX4rwh9yONHIaTzr4WXVhvU9m7ri9"
+type Credentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
 
-	config := oauth1.NewConfig(consumerKey, consumerSecret)
-	token := oauth1.NewToken(accessToken, accessSecret)
+func getClient(creds *Credentials) (*twitter.Client, error) {
+	// Pass in your consumer key (API Key) and your Consumer Secret (API Secret)
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	// Pass in your Access Token and your Access Token Secret
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
 
-	// OAuth1 http.Client will automatically authorize Requests
 	httpClient := config.Client(oauth1.NoContext, token)
-
-	// Twitter client
 	client := twitter.NewClient(httpClient)
 
 	// Verify Credentials
 	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
 		IncludeEmail: twitter.Bool(true),
 	}
-	user, _, _ := client.Accounts.VerifyCredentials(verifyParams)
-	fmt.Printf("User's Name:%+v\n", user.Name)
 
-	// send tweet
-	// Send a Tweet
-	tweet, resp, err := client.Statuses.Update("First tweets test", nil)
-
+	// we can retrieve the user and verify if the credentials
+	// we have used successfully allow us to log in!
+	_, _, err := client.Accounts.VerifyCredentials(verifyParams)
 	if err != nil {
-		log.Fatal(err)
-
+		return nil, err
 	}
-	fmt.Println(tweet)
-	fmt.Println(resp)
+
+	// log.Printf("User's ACCOUNT:\n%+v\n", user)
+	return client, nil
+}
+
+func getAppClient(creds *Credentials) (*twitter.Client, error) {
+
+	// oauth2 configures a client that uses app credentials to keep a fresh token
+	config := &clientcredentials.Config{
+		ClientID:     creds.ConsumerKey,
+		ClientSecret: creds.ConsumerSecret,
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth2.NoContext)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	return client, nil
+
+}
+
+func main() {
+	consumerKey := "o37I7QrJe6Jf7DqRuT7hHeuCo"
+	consumerSecret := "1HglkuCH30gceeSPGiFeSS4j5m3WGkUEeE92P09eN2tnUAJxKU"
+	accessToken := "1508039357898317828-CI5Mj4vMp9i21qy5FsXQCxrU5fmBjP"
+	accessSecret := "mgoyZI5MLoijqGNyBEgOpNkj9zekD9EU2SO1jJZs0rh6R"
+
+	fmt.Println("Go-Twitter Bot v0.01")
+
+	creds := Credentials{
+		AccessToken:       accessToken,
+		AccessTokenSecret: accessSecret,
+		ConsumerKey:       consumerKey,
+		ConsumerSecret:    consumerSecret,
+	}
+
+	fmt.Printf("%+v\n", creds)
+
+	client, err := getClient(&creds)
+	if err != nil {
+		log.Println("Error getting Twitter Client")
+		log.Println(err)
+	}
+
+	// Print out the pointer to our client
+	// for now so it doesn't throw errors
+	//fmt.Printf("%+v\n", client)
+
+	tweet, resp, err := client.Statuses.Update("Habit is a second nature", nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("%+v\n\n\n", resp)
+	log.Printf("%+v\n", tweet)
 
 }
