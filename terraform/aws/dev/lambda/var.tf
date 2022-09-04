ee8ba5a60306438664643612d51e1fd66898c2fa
akhettar <ayache@cirta.dev>
2022-09-04 10:33:50 +0300

    adding terraform

---
+++ terraform/aws/dev/lambda/var.tf
@@ -0,0 +1,36 @@
+
+variable "app_name" {
+  description = "Application name"
+  default     = "twitter-bot-lambda"
+}
+
+variable "app_env" {
+  description = "Application environment tag"
+  default     = "dev"
+}
+
+variable "aws_profile" {
+  description = "Given name in the credential file"
+  type        = string
+  default     = "rahul-admin"
+}
+
+variable "shared_credentials_file" {
+  description = "Profile file with credentials to the AWS account"
+  type        = string
+  default     = "~/.aws/credentials"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default = {
+    application = "Learning-Tutor"
+    env         = "Test"
+  }
+}
+
+variable "s3_arn" {
+  description = "The S3 bucket arn created by the s3 module"
+  type = string
+}
