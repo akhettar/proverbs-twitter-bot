09bd0bc06fb76d8f57d555cad450040a8619a422
akhettar <ayache@cirta.dev>
2022-09-04 10:41:20 +0300

    adding terraform

---
+++ terraform/prod/s3/var.tf
@@ -0,0 +1,18 @@
+variable "bucket_name" {
+    default = "en-proverbs-cirta"
+}
+
+variable "acl_value" {
+    default = "private"
+}
+
+
+/* variable "aws_access_key" {
+default = “<your_access_key>”
+}
+variable "aws_secret_key" {
+default = “<your_secret_key>”
+ } */
+variable "region" {
+    default  = "eu-central-1"
+}
